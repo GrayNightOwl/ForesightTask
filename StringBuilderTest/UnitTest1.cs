5466cb40e1c7d8aff5c68ab52036d3f4382e8df9
Сергей <serega-k97@yandex.ru>
2018-09-07 11:35:51 +0500

    Добавьте файлы проекта.

---
+++ StringBuilderTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace StringBuilderTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            StringBuilder sb = new StringBuilder();
+
+            // act
+            var res = calc.Sum(2, 5);
+
+            // assert
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ TaskForForsait/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForForsait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder("Привет, мир");
+            Console.WriteLine("Длина " + sb.Length);
+            Console.WriteLine("Объём " + sb.Capacity);
+            sb.Append(", счастья тебе");
+            Console.WriteLine("Длина " + sb.Length);
+            Console.WriteLine("Объём " + sb.Capacity);
+            sb.Append(", счастья тебе");
+            Console.WriteLine("Длина " + sb.Length);
+            Console.WriteLine("Объём " + sb.Capacity);
+            Console.ReadLine();
+        }
+    }
+}
